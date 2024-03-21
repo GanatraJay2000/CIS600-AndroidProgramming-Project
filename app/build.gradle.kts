f192789736354d3cddbd3c250470ef3e2557e6bc
Jay Ganatra <ganatrajay2000@gmail.com>
2024-03-20 20:16:41 -0400

    Final

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,15 +2,14 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
-    id("kotlin-kapt")
 }
 
 android {
-    namespace = "com.example.project"
+    namespace = "com.example.travelease"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.project"
+        applicationId = "com.example.travelease"
         minSdk = 30
         targetSdk = 34
         versionCode = 1
@@ -38,13 +37,11 @@ android {
     buildFeatures {
         viewBinding = true
     }
-
 }
-
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -59,25 +56,24 @@ dependencies {
     implementation("com.google.firebase:firebase-storage")
     implementation("com.google.firebase:firebase-common-ktx:20.4.2")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.annotation:annotation:1.6.0")
 
-    
     implementation("com.google.android.libraries.places:places:3.3.0")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
+
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+
+    implementation("com.google.maps.android:maps-ktx:5.0.0")
+    implementation("com.google.maps.android:maps-utils-ktx:5.0.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
 
     implementation("io.coil-kt:coil:2.3.0")
     implementation("androidx.privacysandbox.tools:tools-core:1.0.0-alpha06")
-    implementation("androidx.room:room-runtime:2.6.1")
-    annotationProcessor("androidx.room:room-compiler:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
-
 
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("com.google.maps.android:maps-ktx:5.0.0")
-    implementation("com.google.maps.android:maps-utils-ktx:5.0.0")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
